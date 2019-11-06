79bc5700cf148e2f5845da6fb322166ad69dabee
sharath <hi@sharath.pro>
2019-11-06 01:56:02 -0500

    more scripts for gypsum

--- scripts/GTA_gypsum.sh
+++ scripts/GTA_gypsum.sh
@@ -1,24 +1,26 @@
 #!/bin/sh
 
-sbatch -p 1080ti-long --gres=gpu:1 python3 -m GTA --baseline
-sbatch -p 1080ti-long --gres=gpu:1 python3 -m GTA --baseline
-sbatch -p 1080ti-long --gres=gpu:1 python3 -m GTA --baseline
-sbatch -p 1080ti-long --gres=gpu:1 python3 -m GTA --baseline
-sbatch -p 1080ti-long --gres=gpu:1 python3 -m GTA --baseline
-sbatch -p 1080ti-long --gres=gpu:1 python3 -m GTA --baseline
-sbatch -p 1080ti-long --gres=gpu:1 python3 -m GTA --baseline
-sbatch -p 1080ti-long --gres=gpu:1 python3 -m GTA --baseline
-sbatch -p 1080ti-long --gres=gpu:1 python3 -m GTA --baseline
-sbatch -p 1080ti-long --gres=gpu:1 python3 -m GTA --baseline
 
-sbatch -p 1080ti-long --gres=gpu:1 python3 -m GTA
-sbatch -p 1080ti-long --gres=gpu:1 python3 -m GTA
-sbatch -p 1080ti-long --gres=gpu:1 python3 -m GTA
-sbatch -p 1080ti-long --gres=gpu:1 python3 -m GTA
-sbatch -p 1080ti-long --gres=gpu:1 python3 -m GTA
-sbatch -p 1080ti-long --gres=gpu:1 python3 -m GTA
-sbatch -p 1080ti-long --gres=gpu:1 python3 -m GTA
-sbatch -p 1080ti-long --gres=gpu:1 python3 -m GTA
-sbatch -p 1080ti-long --gres=gpu:1 python3 -m GTA
-sbatch -p 1080ti-long --gres=gpu:1 python3 -m GTA
+sbatch -p 1080ti-long --gres=gpu:1 scripts/launch_GTA_basline.sh
+sbatch -p 1080ti-long --gres=gpu:1 scripts/launch_GTA_basline.sh
+sbatch -p 1080ti-long --gres=gpu:1 scripts/launch_GTA_basline.sh
+sbatch -p 1080ti-long --gres=gpu:1 scripts/launch_GTA_basline.sh
+sbatch -p 1080ti-long --gres=gpu:1 scripts/launch_GTA_basline.sh
+sbatch -p 1080ti-long --gres=gpu:1 scripts/launch_GTA_basline.sh
+sbatch -p 1080ti-long --gres=gpu:1 scripts/launch_GTA_basline.sh
+sbatch -p 1080ti-long --gres=gpu:1 scripts/launch_GTA_basline.sh
+sbatch -p 1080ti-long --gres=gpu:1 scripts/launch_GTA_basline.sh
+sbatch -p 1080ti-long --gres=gpu:1 scripts/launch_GTA_basline.sh
+
+sbatch -p 1080ti-long --gres=gpu:1 scripts/launch_GTA_adapt.sh
+sbatch -p 1080ti-long --gres=gpu:1 scripts/launch_GTA_adapt.sh
+sbatch -p 1080ti-long --gres=gpu:1 scripts/launch_GTA_adapt.sh
+sbatch -p 1080ti-long --gres=gpu:1 scripts/launch_GTA_adapt.sh
+sbatch -p 1080ti-long --gres=gpu:1 scripts/launch_GTA_adapt.sh
+sbatch -p 1080ti-long --gres=gpu:1 scripts/launch_GTA_adapt.sh
+sbatch -p 1080ti-long --gres=gpu:1 scripts/launch_GTA_adapt.sh
+sbatch -p 1080ti-long --gres=gpu:1 scripts/launch_GTA_adapt.sh
+sbatch -p 1080ti-long --gres=gpu:1 scripts/launch_GTA_adapt.sh
+sbatch -p 1080ti-long --gres=gpu:1 scripts/launch_GTA_adapt.sh
+
 
