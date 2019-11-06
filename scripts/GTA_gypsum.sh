3806bea94a917163855bdb2c59aa8adee4dca3f4
sharath <hi@sharath.pro>
2019-11-06 04:16:19 -0500

    remove slurm output files

--- scripts/GTA_gypsum.sh
+++ scripts/GTA_gypsum.sh
@@ -2,6 +2,6 @@
 
 for i in {1..20}
 do
-  sbatch -p 1080ti-long --gres=gpu:1 scripts/launch_GTA.sh
+  sbatch --output=/dev/null --error=/dev/null -p 1080ti-long --gres=gpu:1 scripts/launch_GTA.sh
   sleep 1
 done
