9436f7f87ff66069a49747a797684e87e99cd7e1
sharath <hi@sharath.pro>
2020-03-02 14:07:35 -0500

    increase memory limit

--- scripts/GTA_gypsum.sh
+++ scripts/GTA_gypsum.sh
@@ -7,7 +7,7 @@ for a in ${vals[@]}
 do
   for b in ${vals[@]}
   do
-    sbatch --output=/dev/null --error=/dev/null -p 2080ti-long --gres=gpu:1 scripts/launch_GTA.sh $seed $a $b
-    sleep 1
+    sbatch --output=/dev/null --error=/dev/null -p 2080ti-long --gres=gpu:1 --mem=6144 scripts/launch_GTA.sh $seed $a $b
+    sleep 2
   done
 done
