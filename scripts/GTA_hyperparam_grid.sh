de9f91b9e6374da290bf041a1b0ff53c434f2360
sharath <hi@sharath.pro>
2020-03-02 14:53:56 -0500

    new values for hyperparam search

---
+++ scripts/GTA_hyperparam_grid.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+alphas=(0.0 0.05 0.1 0.15 0.2)
+betas=(0.0 0.01 0.02 0.03 0.04 0.05)
+seed=1
+
+for a in ${alphas[@]}
+do
+  for b in ${betas[@]}
+  do
+    sbatch --output=/dev/null --error=/dev/null -p 2080ti-short --gres=gpu:1 --mem=6144 scripts/launch_GTA_single.sh $seed $a $b
+    sleep 2
+  done
+done
---
+++ scripts/launch_GTA_single.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+seed=$1
+alpha=$2
+beta=$3
+
+python3 -m GTA --seed $1 --alpha $alpha --adv_weight $beta
