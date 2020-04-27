a0ec7bf8c6075191a16b6b503aa0c12a82200d99
sharath <hi@sharath.pro>
2020-04-27 12:57:14 -0400

    add gypsum script for hyperparam search

--- scripts/GTA_hyperparam_grid.sh
+++ scripts/GTA_hyperparam_grid.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 mkdir output
 
-alphas=(0.0 0.05 0.1 0.15 0.2)
-betas=(0.0 0.01 0.02 0.03 0.04 0.05)
-seeds=(0 1 2 3 4)
+alphas=(0.04 0.07 0.1 0.13 0.16)
+betas=(0.01 0.02 0.03 0.04 0.05)
+seeds=(6 7 8)
 
 for a in ${alphas[@]}
 do
@@ -11,8 +11,7 @@ do
   do
     for s in ${seeds[@]}
     do
-      sbatch scripts/launch_GTA_single.sh $s $a $b
-      sleep 2
+      sbatch scripts/launch_GTA_tune_wgan.sh $s $a $b
     done
   done
 done
---
+++ scripts/launch_GTA_tune_wgan.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+#
+#SBATCH --partition=1080ti-long
+#SBATCH --gres=gpu:1
+#SBATCH --mem=8192
+#SBATCH --account=rkozma
+#SBATCH --output=output/ann_%j.out
+
+seed=$1
+alpha=$2
+beta=$3
+
+python3 -m GTA --seed $seed --alpha $alpha --beta $beta
+python3 -m GTA --seed $seed --alpha $alpha --beta -$beta
