e96b25df55d43b9ec3e0d8890014fcd7a5403e15
sharath <hi@sharath.pro>
2020-04-28 11:35:49 -0400

    remove sn and bn

--- GTA/models.py
+++ GTA/models.py
@@ -5,6 +5,24 @@ from torch.nn.utils import spectral_norm as SN
 from .layers import ACModule
 from .utils import init_weights
 
+class ACModule(nn.Module):
+    def __init__(self, filters, nclasses):
+        super(ACModule, self).__init__()
+        self.filters = filters
+        self.classifier_c = nn.Sequential(
+            nn.Linear(filters*2, nclasses)
+        )              
+        self.classifier_s = nn.Sequential(
+            nn.Linear(filters*2, 1), 
+            nn.Sigmoid()
+        )
+    def forward(self, x):
+        real_prob = self.classifier_s(x.view(-1, self.filters*2)).view(-1)
+        class_prob = self.classifier_c(x.view(-1, self.filters*2))
+        return real_prob, class_prob
+    
+    
+
 def get_generator(latent_dim, embedding_dim, filters, nclasses, lr, betas, device):
     G = nn.Sequential(
         nn.ConvTranspose2d(latent_dim + embedding_dim + nclasses+1, filters*8, 2, 1, 0, bias=False),
@@ -34,25 +52,25 @@ def get_generator(latent_dim, embedding_dim, filters, nclasses, lr, betas, devic
 
 def get_discriminator(filters, nclasses, lr, betas, device):
     D = nn.Sequential(
-        SN(nn.Conv2d(3, filters, 3, 1, 1)),            
+        nn.Conv2d(3, filters, 3, 1, 1),
         #nn.BatchNorm2d(filters),     
         nn.LayerNorm(32, 32),
         nn.LeakyReLU(0.2, inplace=True),
         nn.MaxPool2d(2,2),
     
-        SN(nn.Conv2d(filters, filters*2, 3, 1, 1)),         
+        nn.Conv2d(filters, filters*2, 3, 1, 1),
         #nn.BatchNorm2d(filters*2),         
         nn.LayerNorm(16, 16),
         nn.LeakyReLU(0.2, inplace=True),
         nn.MaxPool2d(2,2),
         
-        SN(nn.Conv2d(filters*2, filters*4, 3, 1, 1)),           
+        nn.Conv2d(filters*2, filters*4, 3, 1, 1),
         #nn.BatchNorm2d(filters*4),           
         nn.LayerNorm(8, 8),
         nn.LeakyReLU(0.2, inplace=True),
         nn.MaxPool2d(2,2),
         
-        SN(nn.Conv2d(filters*4, filters*2, 3, 1, 1)),           
+        nn.Conv2d(filters*4, filters*2, 3, 1, 1),
         #nn.BatchNorm2d(filters*2),           
         nn.LayerNorm(4, 4),
         nn.LeakyReLU(0.2, inplace=True),
@@ -68,17 +86,14 @@ def get_discriminator(filters, nclasses, lr, betas, device):
 def get_encoder(channels, filters, embedding_dim, lr, betas, device):
     F = nn.Sequential(
         nn.Conv2d(channels, filters, 5, 1, 0),
-        nn.BatchNorm2d(filters),
         nn.ReLU(inplace=True),
         nn.MaxPool2d(2, 2),
         
         nn.Conv2d(filters, filters, 5, 1, 0),
-        nn.BatchNorm2d(filters),
         nn.ReLU(inplace=True),
         nn.MaxPool2d(2, 2),
         
         nn.Conv2d(filters, embedding_dim, 5, 1,0),
-        nn.BatchNorm2d(embedding_dim),
         nn.ReLU(inplace=True)
     ).apply(init_weights).to(device)
     
@@ -90,7 +105,6 @@ def get_encoder(channels, filters, embedding_dim, lr, betas, device):
 def get_classifier(filters, nclasses, lr, betas, device):
     C = nn.Sequential(
         nn.Linear(2*filters, 2*filters),
-        nn.BatchNorm1d(2*filters),
         nn.ReLU(inplace=True),
         nn.Linear(2*filters, nclasses),      
     ).apply(init_weights).to(device)
